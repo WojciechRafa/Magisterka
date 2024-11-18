76b18148ae74ccef86f4f2f7126c6ee550752df1
Wojciech <wojciechrafa@student.agh.edu.pl>
2024-11-18 22:44:28 +0100

    Fix time read

--- Polaczenie-sieciowe_dla-RPi/Image_source.cpp
+++ Polaczenie-sieciowe_dla-RPi/Image_source.cpp
@@ -26,22 +26,27 @@ Image_source::Image_source(Frame_switching switching_method_,
 void Image_source::update()
 {
     sf::Time main_clock_status = clock.getElapsedTime() - main_clock_diff;
+    static sf::Time last_update_main_time = sf::microseconds(-1);
 
+    sf::Time time_remainder = main_clock_status % Configs::camera_update_period;
+    sf::Time time_nominal = main_clock_status - time_remainder;
     if(switching_method == Frame_switching::automatic){
-        sf::Time time_remainder = main_clock_status % Configs::camera_update_period;
+        if( time_remainder < Configs::max_camera_update_period_error and
+            time_nominal != last_update_main_time){
+            last_update_main_time = time_nominal;
 
-        if(time_remainder > - Configs::max_camera_update_period_error and
-           time_remainder <  Configs::max_camera_update_period_error){
             cap >> last_with_main_time_frame->second;
-            last_with_main_time_frame->first = main_clock_status;
+            last_with_main_time_frame->first = time_nominal;
         }
     }else{
         static bool was_action_button_released = true;
 
         if(was_action_button_released and sf::Keyboard::isKeyPressed(action_key)){
+            last_update_main_time = time_nominal;
+
             was_action_button_released = false;
             cap >> last_with_main_time_frame->second;
-            last_with_main_time_frame->first = main_clock_status;
+            last_with_main_time_frame->first = time_nominal;
         }
 
     }
